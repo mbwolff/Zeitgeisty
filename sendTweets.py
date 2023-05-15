08ca42f855f65ca718b34c8a52b80ffd56ff062a
Mark Wolff <wolff.mark.b@gmail.com>
2023-05-14 22:54:50 -0400

    Update sendTweets.py

--- sendTweets.py
+++ sendTweets.py
@@ -8,14 +8,14 @@ this notice are preserved. This file is offered as-is, without any warranty.
 
 # The file twitter_credsPost.py needs to be created in order to use tweepy
 # and access Twitter.
-from twitter_credsPost import bearer_token, consumer_key, consumer_secret, access_token, access_token_secret
+from twitter_credsPost import consumer_key, consumer_secret, access_token, access_token_secret
 from config import aphorisms_dir, package_dir
 from time import time, sleep
 import os, re, tweepy
 
 authenticate = tweepy.OAuthHandler(consumer_key, consumer_secret)
 authenticate.set_access_token(access_token, access_token_secret)
-api = tweepy.API(authenticate)
+api = tweepy.API(authenticate, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
 aphor_files = os.listdir(aphorisms_dir)
 aphor_files.sort()
@@ -29,5 +29,5 @@ for aphor in aphors:
         break
     elif not re.search('are the aphorisms', aphor) and re.search('\w', aphor):
 #        print(aphor.rstrip())
-        api.update_status(aphor.rstrip())
+        api.update_status(status=aphor.rstrip())
         sleep(900) # wait 15 minutes
